ff379a99046ff508f24540f543adf212ddc73add
İlkay Kesici <ilkayksc@gmail.com>
2022-08-13 15:50:44 +0300

    Interest Calculator

---
+++ PaycoreBootcampProject/Controllers/InterestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+
+namespace PaycoreBootcampProject.Controllers
+{
+    public class Interest
+    {
+        public double principal { get; set; }
+        public double interestRate { get; set; }
+        public int expiry { get; set; }
+      
+        public Interest() { }
+    }
+    public class InterestGain 
+    {
+        
+        public double InterestAmount { get; set; }
+        public double TotalBalance { get; set; }
+        public InterestGain() { }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post([FromQuery] Interest request)
+        {
+            if (request.expiry<0 || request.interestRate<0 ||request.principal<0)
+            {
+                return BadRequest(request);
+            };
+
+            InterestGain interest = new InterestGain();
+            
+            interest.TotalBalance = request.principal * Math.Pow((1+(request.interestRate / 100)),request.expiry);
+            interest.InterestAmount = interest.TotalBalance - request.principal;
+            return Ok(interest);
+        }
+    }
+}
